59116411c4f252333775eb8e53076d9c0199a252
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-11-08 20:18:10 +0200

    Update settings.py

--- django_project_deployment/settings.py
+++ django_project_deployment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dqpx#p^+$)i6mfedel39ur%3d0#zi+tf+c20+-38jiecp&efpy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
